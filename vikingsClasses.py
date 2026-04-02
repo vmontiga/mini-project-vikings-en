7385104c6490cc814407f1950a2eccad55c4035b
allyssa86-max <allyssa86@hotmail.com>
2026-04-02 18:49:25 +0100

    saxon and soldier done

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -5,56 +5,64 @@ import random
 
 class Soldier:
     def __init__(self, health, strength):
-        # your code here
+        self.health = health
+        self.strength = strength
     
     def attack(self):
-        # your code here
-
-    def receiveDamage(self, damage):
-        # your code here
+        return self.strength
     
+    def receiveDamage(self, damage):
+        self.health -= damage
 
 # Viking
 
-class Viking(Soldier):
-    def __init__(self, name, health, strength):
-        # your code here
+# class Viking(Soldier):
+#     def __init__(self, name, health, strength):
+#         # your code here
 
-    def battleCry(self):
-        # your code here
+#     def battleCry(self):
+#         # your code here
 
-    def receiveDamage(self, damage):
-        # your code here
+#     def receiveDamage(self, damage):
+#         # your code here
 
 # Saxon
 
 class Saxon(Soldier):
     def __init__(self, health, strength):
-        # your code here
+        super().__init__(health, strength)
 
     def receiveDamage(self, damage):
-        # your code here
+        self.health -= damage
+        if self.health > 0:
+            return f"A Saxon has received {damage} points of damage"
+        else:
+            return f"A Saxon has died in combat"    
 
 # Davicente
 
 class War():
     def __init__(self):
         # your code here
-
+        pass
     def addViking(self, viking):
         # your code here
+        pass
     
     def addSaxon(self, saxon):
         # your code here
-    
+        pass
+
     def vikingAttack(self):
         # your code here
-    
+        pass
+
     def saxonAttack(self):
         # your code here
+        pass
 
     def showStatus(self):
         # your code here
-    pass
-
+        pass
+    
 
