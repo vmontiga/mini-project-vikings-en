f70b8ee2b61edeaedd1be781e352b75a55c308a2
NelsonP55 <nelsonportela55@gmail.com>
2026-04-02 17:55:37 +0100

    Begin

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -5,13 +5,14 @@ import random
 
 class Soldier:
     def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
       
-
     def attack(self):
+        return self.strength
        
-        
-
     def receiveDamage(self, damage):
+        self.health -= damage
        
     
 
